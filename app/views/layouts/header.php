5fd921415b841d0301a98c750c778a8d960b4017
Alexis <garibaldialexis@gmail.com>
2024-10-30 16:28:38 +0100

    hotfixes

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title><?= isset($title) ? htmlspecialchars($title) : 'Y'; ?></title>
+    <title><?= isset($title) ? htmlspecialchars($title) : 'Z'; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="<?= url('images/favicon.ico'); ?>">
     <link rel="stylesheet" href="<?= url('css/header.css'); ?>">
