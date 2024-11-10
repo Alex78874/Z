de584a62efbbac464240187241873a6771a94b9e
Alexis <garibaldialexis@gmail.com>
2024-11-10 22:37:51 +0100

    hotfixes

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,6 @@
 <!-- Titre -->
 <div class="scroll-up">
-    <button class="button-scroll-up" aria-label="Scroll to top"></button>
+    <button class="button-scroll-up" aria-label="Scroll to top">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up"
             viewBox="0 0 16 16">
             <path fill-rule="evenodd"
